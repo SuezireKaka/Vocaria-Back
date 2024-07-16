61f96b6fbad8170e910b75084bf9126a181be12e
SuezireKaka <rwccm3241@naver.com>
2024-07-16 18:59:40 +0900

    Vocaria 셋업

---
+++ vocaria/src/main/java/www/voca/ria/VocariaApplication.java
@@ -0,0 +1,13 @@
+package www.voca.ria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocariaApplication.class, args);
+	}
+
+}
---
+++ vocaria/src/test/java/www/voca/ria/VocariaApplicationTests.java
@@ -0,0 +1,13 @@
+package www.voca.ria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
