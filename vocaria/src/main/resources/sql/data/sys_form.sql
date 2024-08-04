480327088439a49266d857aeb3d4ffff64da8062
SuezireKaka <rwccm3241@naver.com>
2024-08-04 13:00:24 +0900

    패스워드 type 용어를 프론트에서 그대로 쓸 수 있게 변경

--- vocaria/src/main/resources/sql/data/sys_form.sql
+++ vocaria/src/main/resources/sql/data/sys_form.sql
@@ -2,6 +2,6 @@ insert into sys_form(purpose, level, input_title, input_type, input_valid_regex,
 values ('register', 0, '이름', 'text', '^.{,255}$', 0),
 ('register', 1, '생년월일', 'date', null, 0),
 ('register', 2, '아이디', 'text', '^[A-Za-z0-9]{8,30}$', 1),
-('register', 3, '패스워드', 'pswd', '^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-z0-9#?!@$%^&*-]{8,30}$', 0),
+('register', 3, '패스워드', 'password', '^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-z0-9#?!@$%^&*-]{8,30}$', 0),
 ('register', 4, '닉네임', 'text', '^.{,255}$', 1),
 ('register', 5, '소개', 'text', '^.{,1000}$', 0)
