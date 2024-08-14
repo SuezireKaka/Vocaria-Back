5dd70776ddd1cebeebefefa8c9559eef599a9104
SuezireKaka <rwccm3241@naver.com>
2024-08-14 15:45:39 +0900

    학습정보 테이블의 계정과 단어를 primary key로 묶기

--- vocaria/src/main/resources/sql/table/t_educate.sql
+++ vocaria/src/main/resources/sql/table/t_educate.sql
@@ -1,7 +1,6 @@
 create table t_educate(
 	account			varchar(30),
 	word			varchar(255),
-	viewcnt			int
+	viewcnt			int,
+	primary key (account, word)
 );
-create index idx_educated_account on t_educate(account);
-create index idx_educated_word on t_educate(word);
