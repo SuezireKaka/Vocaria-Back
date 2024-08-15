5bd3546a33521ad7251e85ad8c1bf092a82af55f
SuezireKaka <rwccm3241@naver.com>
2024-08-15 16:53:09 +0900

    활동 추가 및 역할 갱신

--- vocaria/src/main/resources/sql/data/sys_act.sql
+++ vocaria/src/main/resources/sql/data/sys_act.sql
@@ -1,3 +1,5 @@
 insert into sys_act(act, area, type, info)
 values ('SM', 'System', 'Manage', '시스템 전체를 관리합니다'),
-('PM', 'Private', 'Manage', '자신에 대한 모든 것을 관리합니다')
+('ST', 'System', 'Test', '시스템이 잘 돌아가는지 테스트합니다'),
+('PM', 'Personal', 'Manage', '자신에 대한 모든 것을 관리합니다'),
+('PS', 'Personal', 'Study', '보카리아를 활용하여 공부를 합니다')
