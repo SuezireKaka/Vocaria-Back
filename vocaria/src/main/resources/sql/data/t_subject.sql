caa382f844c8a73b2ad879a56669e0f4512e014b
SuezireKaka <rwccm3241@naver.com>
2024-08-22 14:17:40 +0900

    신규 데이터 추가

--- vocaria/src/main/resources/sql/data/t_subject.sql
+++ vocaria/src/main/resources/sql/data/t_subject.sql
@@ -1,2 +1,3 @@
 insert into t_subject (id, subject)
-values (NEXT_PK('s_subject'), '포켓몬')
+values (NEXT_PK('s_subject'), '포켓몬'),
+(NEXT_PK('s_subject'), '영어')
--- vocaria/src/main/resources/sql/data/t_voca.sql
+++ vocaria/src/main/resources/sql/data/t_voca.sql
@@ -1,2 +1,3 @@
 insert into t_voca (id, name, maker, intro)
-values (NEXT_PK('s_voca'), 'sample', '0000', '포켓몬에 대한 자작 문제들 모음')
+values (NEXT_PK('s_voca'), 'sample', 'rwccm3241', '포켓몬에 대한 자작 문제들 모음'),
+(NEXT_PK('s_voca'), '능률보카', 'rwccm3241', '능률적으로 단어를 도와주기')
